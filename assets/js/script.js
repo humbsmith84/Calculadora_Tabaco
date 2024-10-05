c9d1d449c7f4ab66d8e5a31d571af8cc802d4eed
Humberto Smith da Silva <93011482+humbsmith84@users.noreply.github.com>
2024-10-05 17:29:48 -0300

    a

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+let display = document.getElementById("display");
+let currentInput = ""
+let currentOperator = ""
+
+
+function appendNumber(value){
+    currentInput += value
+    display.textContent = currentInput
+}
+
+function appendOperator(operator){
+    if(currentInput === "" && operator !== ".") return
+    currentInput += operator;
+    display.textContent = currentInput
+}
+
+function calculate(){
+    try {
+        let result = eval(currentInput)
+        if(!Number.isInteger(result)){
+            result = result.toFixed(3)
+        }
+        currentInput = result;
+        display.textContent = currentInput;
+    } catch(error){
+        display.textContent = "Erro";
+        currentInput = "";
+    }
+}
